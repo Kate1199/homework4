3885dbd4d21ee107b009e1821072bac8510cba90
Kate1199 <kate.kisel99@yandex.by>
2021-01-29 01:13:44 +0300

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Rectangle square = new Rectangle(5, 5);
+        Rectangle rec1 = new Rectangle(5, 7);
+        Rectangle rec2 = new Rectangle(3, 8);
+
+        square.calculateArea();
+        rec1.calculateArea();
+        rec2.calculateArea();
+
+        square.printArea();
+        rec1.printArea();
+        rec2.printArea();
+
+        System.out.println();
+        System.out.println();
+
+        square.printRectangleKind();
+        rec1.printRectangleKind();
+        rec2.printRectangleKind();
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(square.isTheSameRectangle(rec1));
+        System.out.println(rec1.isTheSameRectangle(rec2));
+        System.out.println(rec2.isTheSameRectangle(square));
     }
 }
--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class Rectangle {
+    double x;
+    double y;
+
+    public Rectangle(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Rectangle(double x) {
+        this.x = x;
+        this.y = x;
+    }
+
+    public double calculateArea() {
+        return x * y;
+    }
+
+    public void printArea() {
+        System.out.println(calculateArea());
+    }
+
+    public void printRectangleKind() {
+        if (x == y)
+            System.out.println("Это квадрат");
+        else
+            System.out.println("Это прямоугольник");
+    }
+
+    public boolean isTheSameRectangle(Rectangle rectangle) {
+        return x == rectangle.x && y == rectangle.y;
+    }
 }
